bbceaf1c3a1fdc915c589a4bcc84e6439a5a1547
heekweon kang <heekweonkang@heekweonui-MacBook-Pro.local>
2020-12-02 18:07:19 +0900

    대쉬보드 완성

--- js/script.pub.js
+++ js/script.pub.js
@@ -1,204 +1,22 @@
 (function($) {
-    $.fn.topmenu = function(){
-        var $header = $('.header');
-        var $mbMenu = $('.mb_menu');
-        var $mypage = $('.mypage');
-        var $topmenu = $('.topmenu');
-        var $wrap = $('.wrap');
-        var $topmenuCont = $topmenu.find('>li');
-        if($(document).width() > 960){
-            $topmenuCont.find('.submenu').hide();
-            $mbMenu.show();
-            $mypage.find('.cont').hide();
-            $topmenuCont.on('mouseenter',function(){
-                $('.submenubox').remove();
-                $topmenuCont.find('.submenu').hide();
-                $(this).find('.submenu').show();
-                $header.append('<div class="submenubox"></div>');
-            });
-            $topmenuCont.find('.submenu').on('mouseleave',function(){
-                reset();
-            });
-            $mypage.find('.btn').on('mouseenter',function(){
-                reset();
-                $(this).find('+.cont').stop().fadeIn();
-            });
-            $mypage.on('mouseleave',function(){
-                $(this).find('.cont').stop().fadeOut();
-            });
-            function reset(){
-                $('.submenubox').remove();
-                $topmenuCont.find('.submenu').hide();
-            }
-        }else{
-            $mbMenu.hide();
-            $topmenuCont.find('.submenu').show();
-            $mypage.find('.cont').show();
-            $header.find('.btn_menu').on('click',function(){
-                $mbMenu.fadeIn();
-                $wrap.addClass('hidden');
-
-            });
-            $mbMenu.find('.btn_close').on('click',function(){
-                $mbMenu.fadeOut();
-                $wrap.removeClass('hidden');
-            });
-            
-        }
-    }
-    $.fn.swiper = function(){
-        mainSlider : {
-            var mainSlider = new Swiper('.mainSlider', {
-                loop: true,
-                spaceBetween: 30,
-                effect: 'fade',
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-                autoplay : {
-                    delay: 5000,
-                },
-            });
-        }
-        challengeSlider : {
-            if($(document).width() > 960){
-                var $challenge = $('.recruit_challenge_box .info_box');
-                var $count = $challenge.find('.fraction .count');
-                var $num = $challenge.find('.fraction .num');
-                var $txt = $challenge.find('.txt');
-                var $company = $challenge.find('.company');
-                var $name = $challenge.find('.name');
-                var challengeSlider = new Swiper('.challengeSlider', {
-                    slidesPerView: 'auto',
-                    spaceBetween: 70,
-                    centeredSlides: true,
-                    on : {
-                        init : function(swiper){
-                            var num = swiper.slides.length;
-                            var $active = $('.challengeSlider .swiper-slide-active');
-                            var txt = $active.find('.info .txt').text();
-                            var company = $active.find('.info .company').text();
-                            var name = $active.find('.info .name').text();
-                            $count.text(1);
-                            $num.text(num);
-                            $txt.text(txt);
-                            $company.text(company);
-                            $name.text(name);
-                        },
-                        slideChange : function(swiper){
-                            var idx = swiper.realIndex+1;
-                            $count.text(idx);
-                        },
-                        slideChangeTransitionStart :function(){
-                            var $active = $('.challengeSlider .swiper-slide-active');
-                            var txt = $active.find('.info .txt').text();
-                            var company = $active.find('.info .company').text();
-                            var name = $active.find('.info .name').text();
-                            $txt.text(txt);
-                            $company.text(company);
-                            $name.text(name);
-                        }
-                    }
-                });
-                $challenge.find('.btn_next').on('click',function(){
-                    challengeSlider.slideNext();
-                });
-                $challenge.find('.btn_prev').on('click',function(){
-                    challengeSlider.slidePrev();
-                });
+    $.fn.lnb = function(){
+        $('.lnb_wrap li .menu_cont').hide();
+        $('.lnb_wrap li').eq(0).addClass('on');
+        $('.lnb_wrap li').eq(0).children('.menu_cont').show();
+        $('.lnb_wrap li .menu_tit').on('click', function(){
+            if(!$(this).parent('li').hasClass('on')){
+                $(this).parent('li').addClass('on');
+                $(this).siblings('.menu_cont').stop().slideDown();
+                
             }else{
-                var challengeSlider = new Swiper('.challengeSlider', {
-                    slidesPerView: '1',
-                    centeredSlides: true,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        type: 'bullets',
-                    }
-                });
+                $(this).parent('li').removeClass('on');
+                $(this).siblings('.menu_cont').stop().slideUp();
             }
-        }
-        gallery : {
-            var $gallerybox = $('.gallery_box');
-            var $count2 = $gallerybox.find('.count')
-            var $num2 = $gallerybox.find('.num')
-            var galleryThumbs = new Swiper('.gallery-thumbs', {
-                spaceBetween: 30,
-                slidesPerView:6,
-                freeMode: true,
-                watchSlidesVisibility: true,
-                watchSlidesProgress: true,
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                }
-              });
-              var galleryTop = new Swiper('.gallery-top', {
-                navigation: {
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
-                },
-                thumbs: {
-                  swiper: galleryThumbs
-                },
-                on : {
-                    init : function(){
-                        var num = $('.gallery-top .swiper-slide').length;
-                        $num2.text(num);
-                        $count2.text(1);
-                    },
-                    slideChange : function(swiper){
-                        var idx = swiper.realIndex+1;
-                        $count2.text(idx);
-                    }
-                }
-            });
-            $gallerybox.find('.btn_next').on('click',function(){
-                galleryTop.slideNext();
-            });
-            $gallerybox.find('.btn_prev').on('click',function(){
-                galleryTop.slidePrev();
-            });
-        }
-    }
-    $.fn.map = function(){
-        if($(document).width() > 960){
-            var $locList = $('.cv_location_box .cv_list li');
-            var $mapList = $('.cv_location_box .map_list li');
-            $locList.first().addClass('on');
-            $mapList.first().addClass('on');
-            $locList.on('mouseenter',function(){
-                var idx =$(this).index();
-                $locList.removeClass('on');
-                $mapList.removeClass('on');
-                $(this).addClass('on');
-                $mapList.eq(idx).addClass('on');
-            });
-        }
-    }
-    $.fn.share = function(){
-        var $social = $('.social');
-        $social.each(function(){
-            $(this).find('.btn_share').on('mouseenter',function(){
-                $(this).find('+.sel').addClass('on');
-            });
-            $social.find('.sel').on('mouseleave',function(){
-                $(this).removeClass('on');
-            });
-        });
-    }
-    $.fn.footer = function(){
-        var $family = $('.family_link');
-        $family.find('.btn').on('mouseenter',function(){
-            $(this).find('+.list').show();
-        });
-        $family.on('mouseleave',function(){
-            $(this).find('.list').hide();
         });
     }
     $.fn.tab = function(){
-        var tab_btn = $('.tab_wrap .tab_navi li');
-        var tab_cont = $('.tab_wrap .tab_content');
+        var tab_btn = $('.catagory_wrap .tab_navi li');
+        var tab_cont = $('.catagory_wrap .tab_content');
 
         tab_cont.hide();
         tab_cont.eq(0).show();
@@ -211,29 +29,9 @@
             $(this).addClass('on');
         });
     }
-    $.fn.toggle = function(){
-        $('.btn_toggle button').on('click', function(){
-            if(!$(this).hasClass('on')){
-                $(this).addClass('on');
-                $(this).siblings('button').removeClass('on');
-            }else{
-                $(this).removeClass('on');
-                $(this).siblings('button').addClass('on');
-            }
-        });
-    }
 })(jQuery);
 
 window.onload = function(){
-    $.fn.topmenu();     //topmenu
-    $.fn.swiper();      //swiper
-    $.fn.map();         //map
-    $.fn.share();       //share button
-    $.fn.footer();      //footer
+    $.fn.lnb();         //lnb
     $.fn.tab();         //tab
-    $.fn.toggle();      //toggle
-}
-window.onresize = function(){
-    $.fn.topmenu();     //topmenu
-    $.fn.swiper();      //swiper
 }
