b4251210033bc138c90415ce1fd5c79b06f1aeae
heekweon kang <heekweonkang@heekweonui-MacBook-Pro.local>
2020-12-09 17:46:06 +0900

    2차 디자인 작업완료

--- js/script.pub.js
+++ js/script.pub.js
@@ -57,11 +57,24 @@
             }
         });
     }
+    
+    
+    $.fn.calendar = function(){
+        $( ".datepicker" ).datepicker({
+            changeYear: true,
+            changeMonth: true, 
+            monthNamesShort:[ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" ],
+            dayNamesMin: [ '일', '월', '화', '수', '목', '금', '토'],
+            showMonthAfterYear: true,
+            showOtherMonths: true
+        });
+    }
 })(jQuery);
 
 window.onload = function(){
-    $.fn.lnb();         //lnb
-    $.fn.toggle();         //toggle
-    $.fn.catagory_tab();         //tab
-    $.fn.tab();         //tab
+    $.fn.lnb();             //lnb
+    $.fn.toggle();          //toggle
+    $.fn.catagory_tab();    //catagory_tab
+    $.fn.tab();             //tab
+    $.fn.calendar();        //calendar
 }
